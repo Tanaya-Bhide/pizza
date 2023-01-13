7a40b6f0337cf79b1ac6e90c0acd51c31ceffbbc
tanaya-bhide <bhidetm@gmail.com>
2023-01-13 13:40:18 +0530

    add cart page ,authentication

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-const express = require('express')
-const app = express()
-const ejs = require('ejs')
-const path = require('path')
-const expressLayout = require('express-ejs-layouts')
-const PORT = process.env.PORT || 3000 
-app.use(express.static('public'))
-app.get('/',(req,res)=>{res.render('home')})
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
+const PORT = process.env.PORT || 3000;
+app.use(express.static("public"));
 
-app.use(expressLayout)
-app.set('views', path.join(__dirname,'/resources/views'))
-app.set('view engine','ejs')
-app.listen(3000,()=>{
-    console.log('Listening on 3000')
-})
+app.use(expressLayout);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+app.get("/register", (req, res) => {
+  res.render("auth/register");
+});
+app.listen(3000, () => {
+  console.log("Listening on 3000");
+});
