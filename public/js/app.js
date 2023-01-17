a94dad335c70cf3a61b72ff91f3cc1df1fc5a63a
tanaya-bhide <bhidetm@gmail.com>
2023-01-17 20:49:24 +0530

    first commit

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,22 @@
   \*********************************/
 /***/ (() => {
 
-console.log("hellooooooooo");
+/*import axios from "axios";
+let addtoCart = document.querySelectorAll(".add-to-Cart");
+ let cartCounter = document.querySelectorAll("#cartCounter");
+function updateCart(pizza) {
+  axios.post("/update-cart", pizza).then((res) => {
+    console.log(res);
+    cartCounter.innertext = res.data.totalQty;
+  });
+}
+
+addtoCart.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let pizza = JSON.parse(btn.dataset.pizza);
+    updateCart(pizza);
+  });
+});*/
 
 /***/ }),
 
--- server.js
+++ server.js
@@ -1,28 +1,57 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const path = require("path");
 const expressLayout = require("express-ejs-layouts");
 const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+
+const url = "mongodb://localhost/pizza";
+ const flash = require("express-flash");
+const session = require("express-session");
+const MongoDbStore = require("connect-mongo")(session);
+//app.use(express.json());
+mongoose.set("strictQuery", false);
+mongoose.connect(url, {
+  useNewUrlParser: true,
+
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection
+  .once("open", () => {
+    console.log("Database connected...");
+  })
+  .on("error", function (err) {
+    console.log("connection failed");
+  });
+
+let mongoStore = new MongoDbStore({
+  mongooseConnection: connection,
+  collection: "sessions",
+});
+
+
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: mongoStore,
+    cookie: { maxAge: 1000 * 15 },
+  })
+);
+
 app.use(express.static("public"));
 
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-app.get("/cart", (req, res) => {
-  res.render("customers/cart");
-});
+require("./routes/web")(app);
 
-app.get("/login", (req, res) => {
-  res.render("auth/login");
-});
-app.get("/register", (req, res) => {
-  res.render("auth/register");
-});
 app.listen(3000, () => {
   console.log("Listening on 3000");
 });
